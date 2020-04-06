1022f6b854fb7a1d409eb60fa113f065be9f594a
Ergin, Huseyin <hergin@bsu.edu>
2020-04-06 12:44:34 -0400

    Another chart.

--- src/main/java/CalculatorUI.java
+++ src/main/java/CalculatorUI.java
@@ -6,18 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class CalculatorUI extends Application implements EventHandler<ActionEvent> {
+import java.util.Random;
+
+public class CalculatorUI extends Application {
 
     Calculator theCalculator = new Calculator();
-
-    Label displayLabel = new Label(theCalculator.getMainNumber());
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -26,40 +29,6 @@ public class CalculatorUI extends Application implements EventHandler<ActionEven
         GridPane gridPane = new GridPane();
 
 
-        displayLabel.setFont(new Font(20));
-        displayLabel.setPadding(new Insets(20));
-        displayLabel.setPrefSize(200, 100);
-        gridPane.add(displayLabel, 0, 0, 2, 1);
-
-        Button button0 = new Button("0");
-        button0.setOnAction(value -> {
-            theCalculator.appendToMainNumber("0");
-            displayLabel.setText(theCalculator.getMainNumber());
-        });
-        button0.setPrefSize(100, 100);
-        gridPane.add(button0, 0, 1, 1, 1);
-
-        Button button1 = new Button("1");
-        button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                theCalculator.appendToMainNumber("1");
-                displayLabel.setText(theCalculator.getMainNumber());
-            }
-        });
-        button1.setPrefSize(100, 100);
-        gridPane.add(button1, 1, 1, 1, 1);
-
-        Button plusButton = new Button("+");
-        plusButton.setOnAction(this);
-        plusButton.setPrefSize(100, 100);
-        gridPane.add(plusButton, 0, 2, 1, 1);
-
-        Button equalButton = new Button("=");
-        equalButton.setOnAction(this);
-        equalButton.setPrefSize(100, 100);
-        gridPane.add(equalButton, 1, 2, 1, 1);
-
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
                         new PieChart.Data("Grapefruit", 13),
@@ -67,23 +36,52 @@ public class CalculatorUI extends Application implements EventHandler<ActionEven
                         new PieChart.Data("Plums", 10),
                         new PieChart.Data("Pears", 22),
                         new PieChart.Data("Apples", 30));
-        final PieChart chart = new PieChart(pieChartData);
-        chart.setTitle("Imported Fruits");
-        gridPane.add(chart,0,3,2,1);
+        final PieChart pieChart = new PieChart(pieChartData);
+        pieChart.setTitle("Imported Fruits");
 
-        Scene mainScene = new Scene(gridPane, 300, 600);
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        final LineChart<Number, Number> lineChart =
+                new LineChart<Number, Number>(xAxis, yAxis);
+
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 24));
+        series.getData().add(new XYChart.Data(5, 34));
+        series.getData().add(new XYChart.Data(6, 36));
+        series.getData().add(new XYChart.Data(7, 22));
+        series.getData().add(new XYChart.Data(8, 45));
+        series.getData().add(new XYChart.Data(9, 43));
+        series.getData().add(new XYChart.Data(10, 17));
+        series.getData().add(new XYChart.Data(11, 29));
+        series.getData().add(new XYChart.Data(12, 25));
+
+        lineChart.getData().add(series);
+
+        gridPane.add(lineChart, 0, 3, 2, 1);
+        gridPane.add(pieChart, 0, 4, 2, 1);
+
+        Button plusButton = new Button("Add one more");
+        plusButton.setOnAction(actionEvent -> {
+            var randomNumber = new Random(System.nanoTime()).nextInt(50);
+            series.getData().add(new XYChart.Data(series.getData().size() + 1, randomNumber));
+            pieChartData.add(new PieChart.Data("Another fruit", randomNumber));
+        });
+        plusButton.setPrefSize(100, 50);
+        gridPane.add(plusButton, 0, 0, 1, 3);
+
+        Scene mainScene = new Scene(gridPane, 600, 900);
         stage.setScene(mainScene);
         stage.show();
     }
 
-    @Override
-    public void handle(ActionEvent actionEvent) {
-        if (((Button) actionEvent.getSource()).getText().equals("=")) {
-            theCalculator.add(displayLabel.getText());
-            displayLabel.setText(theCalculator.getMainNumber());
-        } else {
-            theCalculator.storeAndResetMainNumber();
-            displayLabel.setText(theCalculator.getMainNumber());
-        }
-    }
 }
